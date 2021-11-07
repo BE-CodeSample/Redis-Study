595531d02c38fbc8ee9b40b1c0cdfcb620f9dde1
TaeWoonJeong <pig07911@naver.com>
2021-11-08 01:43:15 +0900

    init project

---
+++ Jose/redis/src/test/kotlin/com/jose/redis/RedisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jose.redis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
