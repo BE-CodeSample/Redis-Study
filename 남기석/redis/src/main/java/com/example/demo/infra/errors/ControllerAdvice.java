18cd8f9e5454a3b9abffda9219c8cdcb470d074c
skarltjr <kisa0828@naver.com>
2021-11-12 00:51:48 +0900

    feat(controllerAdvice)  : add controllerAdvice to catch exceptions

--- 남기석/redis/src/main/java/com/example/demo/infra/errors/ControllerAdvice.java
+++ 남기석/redis/src/main/java/com/example/demo/infra/errors/ControllerAdvice.java
@@ -1,8 +1,26 @@
 package com.example.demo.infra.errors;
 
+import com.example.demo.infra.errors.exception.PasswordNotMatchException;
+import com.example.demo.infra.errors.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerAdvice {
     //todo exceptionHandler
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity handleUserNotFound() {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST,
+                ErrorCode.USER_NOT_FOUND.getErrorCode(), ErrorCode.USER_NOT_FOUND.getErrorMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(PasswordNotMatchException.class)
+    public ResponseEntity handlePasswordNotMatch() {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST,
+                ErrorCode.PASSWORD_NOT_MATCH.getErrorCode(), ErrorCode.PASSWORD_NOT_MATCH.getErrorMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
 }
